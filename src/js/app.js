0497193d936b69ca1d77d92725d4a5b85e6fb431
yeion7 <jahir045@gmail.com>
2016-04-13 20:57:21 -0500

    add js

---
+++ src/js/app.js
@@ -0,0 +1,43 @@
+const $ = require('jquery');
+const masonry = require('masonry-layout');
+
+// desplegar menu mobile
+var navMenu = document.getElementById('nav-menu');
+var mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenu.addEventListener('click', function (ev) {
+  ev.preventDefault();
+  navMenu.classList.toggle('show');
+});
+
+// desplegar barra de busqueda
+var searchButton = document.getElementById('search-button');
+var searchInput = document.getElementById('search-input');
+
+searchButton.addEventListener('click', function (ev) {
+  ev.preventDefault();
+  searchInput.classList.toggle('show');
+  mobileMenu.classList.toggle('hiden');
+  navMenu.classList.toggle('hiden');
+  signinButton.classList.toggle('hiden');
+});
+
+//desplegar modal
+
+function toggleClass(target, clase) {
+  console.log(target.classList.toggle(clase));
+};
+
+var signinButton = document.getElementById('signin');
+var modalContainer = document.getElementById('modal');
+var modalButton = document.getElementById('modal-close');
+
+signinButton.addEventListener('click', function (ev) {
+  ev.preventDefault();
+  toggleClass(modalContainer, 'hiden');
+});
+
+modalButton.addEventListener('click', function (ev) {
+  ev.preventDefault();
+  toggleClass(modalContainer, 'hiden');
+});
